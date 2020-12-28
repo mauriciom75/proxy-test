df2da6944f965ed18aaaffd6a1b98526a28910a9
mauriciom75 <mauriciom7575@gmail.com>
2020-12-28 19:08:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const httpProxy = require('http-proxy');
 const proxy     = httpProxy.createProxyServer({});
 
 http.createServer(function(req, res) {
+  console.log("algo llego");
+
+  console.log(req.headers);
+  
   console.log(req.url);
   proxy.web(req, res, { target: req.url });
 }).listen(8080, () => {
