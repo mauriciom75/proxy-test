cc3d93528e9619a0754931ff53e0655f15a39a38
mauriciom75 <mauriciom7575@gmail.com>
2020-12-28 20:33:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const http      = require('http');
 const httpProxy = require('http-proxy');
 const proxy     = httpProxy.createProxyServer({});
 
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  console.log("paso proxyReq");
+  proxyReq.setHeader('X-Special-paso', 'poraca');
+});
+
 http.createServer(function(req, res) {
   console.log("algo llego");
 
